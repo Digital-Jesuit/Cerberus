27f3d0f1575b6b63b8a8c3fe803a840a0d9a0e77
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-03-19 17:17:18 +0100

    Improve UUID lookup and caching in ConnectionManager
    Enhanced ConnectionManager class to support retrieval of a UUID associated with a Minecraft username via a REST API. Also integrated a caching mechanism to store username-to-UUID mapping for one hour to improve performance, reducing the need for repeated network requests. This modification brings more efficient and reliable UUID lookup operations.

--- src/main/java/me/youhavetrouble/cerberus/ConnectionManager.java
+++ src/main/java/me/youhavetrouble/cerberus/ConnectionManager.java
@@ -2,12 +2,20 @@ package me.youhavetrouble.cerberus;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.gson.Gson;
+import com.velocitypowered.api.proxy.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.SecureRandom;
+import java.util.Locale;
+import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class ConnectionManager {
@@ -15,11 +23,20 @@ public class ConnectionManager {
     private final Cerberus plugin;
     private final Random random = new SecureRandom();
 
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    private final Gson gson = new Gson();
+
     private final Cache<String, UUID> connectionUuids = CacheBuilder.newBuilder()
             .expireAfterWrite(3, TimeUnit.MINUTES)
             .build();
     private final Cache<UUID, String> connectionCodes = CacheBuilder.newBuilder()
             .expireAfterWrite(3, TimeUnit.MINUTES)
+            .build();
+
+    private final Cache<String, UUID> nameCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(1, TimeUnit.HOURS)
+            .maximumSize(200)
             .build();
 
     protected ConnectionManager(Cerberus plugin) {
@@ -80,11 +97,49 @@ public class ConnectionManager {
     private String generateCode() {
         StringBuilder builder = new StringBuilder(7);
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             int index = random.nextInt(characters.length());
             builder.append(characters.charAt(index));
         }
         return builder.toString();
+    }
+
+    /**
+     * Retrieves the UUID associated with a Minecraft username.
+     * @param minecraftName The Minecraft username.
+     * @return A CompletableFuture that resolves to the UUID associated with the username,
+     * or null if the username is not found or if there is an error.
+     */
+    public CompletableFuture<UUID> getUuidFromName(@NotNull String minecraftName) {
+        String lowercaseName = minecraftName.toLowerCase(Locale.ENGLISH);
+        UUID playerId = nameCache.getIfPresent(lowercaseName);
+        if (playerId != null) return CompletableFuture.completedFuture(playerId);
+
+        Optional<Player> optionalPlayer = plugin.getServer().getPlayer(lowercaseName);
+        if (optionalPlayer.isPresent()) {
+            Player player = optionalPlayer.get();
+            nameCache.put(lowercaseName, player.getUniqueId());
+            return CompletableFuture.completedFuture(player.getUniqueId());
+        }
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.mojang.com/users/profiles/minecraft/%s".formatted(lowercaseName)))
+                    .build();
+
+            return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(responseBody -> {
+                        UUID id = UUID.fromString(gson.fromJson(responseBody, java.util.Map.class).get("id").toString().replaceFirst(
+                                "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5"
+                        ));
+                        nameCache.put(lowercaseName, id);
+                        return id;
+                    })
+                    .exceptionally(throwable -> null);
+        } catch (URISyntaxException e) {
+            return CompletableFuture.completedFuture(null);
+        }
     }
 
     public enum ConnectionStatus {
